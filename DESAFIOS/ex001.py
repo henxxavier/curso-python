b6af616aff600329423f1c79d554030a1f1baf4a
henxxavier <henxxavier@gmail.com>
2022-10-01 14:34:42 -0300

    resolucao

--- DESAFIOS/ex001.py
+++ DESAFIOS/ex001.py
@@ -1,2 +1,12 @@
-print('Hello World')
-print('Ola Mundo')
+from datetime import date
+
+nome = 'Henrique Xavier'
+idade = 25
+peso = 110
+altura = 1.78
+ano_atual = date.today().year
+ano_nascimento = ano_atual - idade
+imc = peso / (altura * altura)
+
+print(f'{nome} tem {idade} anos e nasceu em {ano_nascimento}')
+print(f'O IMC de {nome} e de {imc}')
