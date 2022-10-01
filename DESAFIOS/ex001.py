b11ef9703ada8039954f834de2add61957c1ce42
henxxavier <henxxavier@gmail.com>
2022-10-01 14:40:02 -0300

    modify

--- DESAFIOS/ex001.py
+++ DESAFIOS/ex001.py
@@ -6,7 +6,8 @@ peso = 110
 altura = 1.78
 ano_atual = date.today().year
 ano_nascimento = ano_atual - idade
-imc = peso / (altura * altura)
+imc = peso / (altura ** 2)
 
-print(f'{nome} tem {idade} anos e nasceu em {ano_nascimento}')
-print(f'O IMC de {nome} e de {imc}')
+print(f'{nome} tem {idade} anos ')
+print(f'pesa {peso}Kg e o IMC de {nome} e de {imc:.2f}')
+print(f'{nome} nasceu em {ano_nascimento}')
