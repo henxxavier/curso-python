b7dbd4fe0e5198dd4e231d163127649807695345
henxxavier <henxxavier@gmail.com>
2022-10-02 05:19:26 -0300

    moddificando alguns erros

--- DESAFIOS/ex001.py
+++ DESAFIOS/ex001.py
@@ -1,6 +1,6 @@
 from datetime import date
 
-nome = 'Henrique Xavier'
+nome = 'Henrique'
 idade = 25
 peso = 110
 altura = 1.78
@@ -8,6 +8,6 @@ ano_atual = date.today().year
 ano_nascimento = ano_atual - idade
 imc = peso / (altura ** 2)
 
-print(f'{nome} tem {idade} anos ')
-print(f'pesa {peso}Kg e o IMC de {nome} e de {imc:.2f}')
+print(f'{nome} tem {idade} anos, ')
+print(f'Pesa {peso}Kg e o IMC de {nome} e de {imc:.2f}')
 print(f'{nome} nasceu em {ano_nascimento}')
