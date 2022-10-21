31814c4a4619f928433211bf9dfb1efa832c3c7a
henxxavier <henxxavier@gmail.com>
2022-10-20 21:58:34 -0300

    desafio concluido.

--- DESAFIOS/ex006.py
+++ DESAFIOS/ex006.py
@@ -6,14 +6,33 @@ while contador > 1:
     
     for valor in cpf[0:9]:
         multiplicacar_cpf = int(valor) * contador
-        print(f'{int(valor)} * {contador} = {multiplicacar_cpf}')
+        # print(f'{int(valor)} * {contador} = {multiplicacar_cpf}')
         acumulador += multiplicacar_cpf
         contador -= 1
 
-print(acumulador)
+        digito_1 = 11 - (acumulador % 11)
+        if digito_1 > 9:
+            digito_1 = 0           
 
-digito_1 = 11 - (acumulador % 11)
-if digito_1 > 9:
-    digito_1 = 0
+contador = 11
+while contador > 1:
     
-print(digito_1)
+    for valor in cpf[0:9]:
+        multiplicacar_cpf = int(valor) * contador
+        # print(f'{int(valor)} * {contador} = {multiplicacar_cpf}')
+        acumulador += multiplicacar_cpf
+        contador -= 1
+
+        digito_2 = 11 - (acumulador % 11)
+        if digito_2 > 9:
+           digito_2 = 0
+
+digito_1 = str(digito_1)
+digito_2 = str(digito_2)
+
+novo_cpf = cpf[0:9] + digito_1 + digito_2
+
+if novo_cpf == cpf:
+    print('CPF VALIDO!')
+else:
+    print('CPF INVALIDO!')
