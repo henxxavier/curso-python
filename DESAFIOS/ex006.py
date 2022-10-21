0ab423cb4160e7e64efa261b0742f2d26b069f2f
henxxavier <henxxavier@gmail.com>
2022-10-20 22:22:16 -0300

    PEP-8

--- DESAFIOS/ex006.py
+++ DESAFIOS/ex006.py
@@ -1,38 +1,41 @@
-contador = 10
-acumulador = 0
-cpf = input('Digite o seu CPF: ')
-
-while contador > 1:
+while True:
+    contador = 10
+    acumulador = 0
+    cpf = input('Digite o seu CPF: ')
     
-    for valor in cpf[0:9]:
-        multiplicacar_cpf = int(valor) * contador
-        # print(f'{int(valor)} * {contador} = {multiplicacar_cpf}')
-        acumulador += multiplicacar_cpf
-        contador -= 1
+    if not cpf.isnumeric():
+        print('Erro Digite apenas numeros')
+        continue
 
-        digito_1 = 11 - (acumulador % 11)
-        if digito_1 > 9:
-            digito_1 = 0           
+    while contador > 1:
+        
+        for valor in cpf[0:9]:
+            multiplicacar_cpf = int(valor) * contador
+            acumulador += multiplicacar_cpf
+            contador -= 1
 
-contador = 11
-while contador > 1:
-    
-    for valor in cpf[0:9]:
-        multiplicacar_cpf = int(valor) * contador
-        # print(f'{int(valor)} * {contador} = {multiplicacar_cpf}')
-        acumulador += multiplicacar_cpf
-        contador -= 1
+            digito_1 = 11 - (acumulador % 11)
+            if digito_1 > 9:
+                digito_1 = 0           
 
-        digito_2 = 11 - (acumulador % 11)
-        if digito_2 > 9:
-           digito_2 = 0
+    contador = 11
+    while contador > 1:
+        
+        for valor in cpf[0:9]:
+            multiplicacar_cpf = int(valor) * contador
+            acumulador += multiplicacar_cpf
+            contador -= 1
 
-digito_1 = str(digito_1)
-digito_2 = str(digito_2)
+            digito_2 = 11 - (acumulador % 11)
+            if digito_2 > 9:
+                digito_2 = 0
 
-novo_cpf = cpf[0:9] + digito_1 + digito_2
+    digito_1 = str(digito_1)
+    digito_2 = str(digito_2)
 
-if novo_cpf == cpf:
-    print('CPF VALIDO!')
-else:
-    print('CPF INVALIDO!')
+    novo_cpf = cpf[0:9] + digito_1 + digito_2
+
+    if novo_cpf == cpf:
+        print('CPF VALIDO!')
+    else:
+        print('CPF INVALIDO!')
