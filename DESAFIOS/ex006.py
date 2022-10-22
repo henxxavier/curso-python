500308f75b1e67880fb13747c8b14ebfca2b6500
henxxavier <henxxavier@gmail.com>
2022-10-22 07:44:43 -0300

    correcao de bugs

--- DESAFIOS/ex006.py
+++ DESAFIOS/ex006.py
@@ -13,28 +13,27 @@ while True:
             multiplicacar_cpf = int(valor) * contador
             acumulador += multiplicacar_cpf
             contador -= 1
-
             digito_1 = 11 - (acumulador % 11)
             if digito_1 > 9:
-                digito_1 = 0           
+                digito_1 = 0          
 
     contador = 11
+    acumulador = 0
+    
     while contador > 1:
-        
-        for valor in cpf[0:9]:
+        for valor in cpf[0:10]:
             multiplicacar_cpf = int(valor) * contador
             acumulador += multiplicacar_cpf
             contador -= 1
-
             digito_2 = 11 - (acumulador % 11)
             if digito_2 > 9:
-                digito_2 = 0
+                digito_2 = 0  
 
-    digito_1 = str(digito_1)
     digito_2 = str(digito_2)
-
+    digito_1 = str(digito_1)
+ 
     novo_cpf = cpf[0:9] + digito_1 + digito_2
-
+    print(cpf, novo_cpf)
     if novo_cpf == cpf:
         print('CPF VALIDO!')
     else:
