bd4e6eab83f05d8b866e6c8ff75102cee9f81555
henxxavier <henxxavier@gmail.com>
2022-10-27 21:18:19 -0300

    editions

---
+++ DESAFIOS/ex007.py
@@ -0,0 +1,7 @@
+nome = input('Digite o seu nome: ')
+
+def Saudacao(msg, nome):
+    print('Bem vindo', nome)
+    
+
+Saudacao('Bem vindo',nome)
---
+++ DESAFIOS/ex008.py
@@ -0,0 +1,10 @@
+n1= int(input('Digite um numero: '))
+n2= int(input('Digite outro numero: '))
+n3= int(input('Digite outro numero: '))
+
+def Soma(x,y,z):
+    return x + y + z
+
+total = Soma(n1,n2,n3)
+
+print(f'O Resultado da soma entre os 3 numeros e {total}')
